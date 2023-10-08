00e2d68e202da1e2fc7e687e6da0f08b80326a82
thomas1996 <thomas.kantahck@gmail.com>
2023-10-08 19:16:24 +0200

    adding progress bar

---
+++ src/loading.js
@@ -0,0 +1,41 @@
+// loading.js
+
+// Sélectionnez l'élément HTML pour le composant de chargement
+const loadingContainer = document.querySelector(".loading-container");
+const progressBar = document.querySelector(".progress-bar");
+const loadingMessage = document.querySelector(".loading-message");
+
+// Créez un gestionnaire de chargement pour les ressources
+const resourceLoader = new ResourceLoader();
+
+// Définissez les ressources à charger ici
+const resourcesToLoad = [
+  "/models/room.glb",
+  "/textures/texture1.png",
+  // Ajoutez d'autres ressources ici
+];
+
+// Variable pour suivre le nombre de ressources chargées
+let loadedResources = 0;
+
+// Fonction de mise à jour de la barre de progression
+function updateProgressBar() {
+  loadedResources++;
+  const progress = (loadedResources / resourcesToLoad.length) * 100;
+  progressBar.style.width = progress + "%";
+
+  // Mettez à jour le message de chargement
+  loadingMessage.textContent = `Chargement : ${progress.toFixed(1)}%`;
+
+  // Si toutes les ressources sont chargées, masquez le composant de chargement
+  if (loadedResources === resourcesToLoad.length) {
+    loadingContainer.style.display = "none";
+  }
+}
+
+// Chargez chaque ressource et mettez à jour la barre de progression
+resourcesToLoad.forEach((resourceUrl) => {
+  resourceLoader.loadResource(resourceUrl, () => {
+    updateProgressBar();
+  });
+});
--- src/script.js
+++ src/script.js
@@ -31,18 +31,33 @@ let mixer = null;
 let objectDistance = 3.5;
 let objects = [];
 
-function loadModel(url) {
+function loadModel(url, onProgressCallback) {
   return new Promise((resolve, reject) => {
-    gltfLoader.load(url, resolve, null, reject);
+    gltfLoader.load(
+      url,
+      resolve,
+      onProgressCallback,
+      reject
+    );
   });
 }
 
 async function loadModels() {
   try {
-    const canap = await loadModel("/models/room.glb");
+    // Chargez le modèle avec le GLTFLoader et suivez le chargement
+    const canap = await loadModel("/models/room.glb", (xhr) => {
+      const percentage = (xhr.loaded / xhr.total) * 100;
+      updateLoadingProgress(percentage);
+    });
+
     canap.scene.position.z = 0;
     objects.push(canap.scene);
     scene.add(canap.scene);
+
+    // Mettre à jour la barre de chargement à 100% une fois que tout est chargé
+    updateLoadingProgress(100);
+    // Afficher la scène une fois que tout est chargé
+    scene.visible = true;
   } catch (error) {
     console.error("Error loading models:", error);
   }
@@ -58,10 +73,11 @@ const walls = new THREE.Mesh(
 );
 
 floor.rotation.x = -Math.PI / 2;
-walls.position.z = -3
+walls.position.z = -3;
 scene.add(floor);
 scene.add(walls);
 
+// Call loadModels to initiate the loading process
 loadModels();
 
 /**
@@ -71,10 +87,10 @@ loadModels();
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, .5);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
 
-const light = new THREE.PointLight(0xFFFFFF, 1, 100);
+const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 2, 1);
 light.castShadow = true; // Activer les ombres pour la lumière
 light.shadow.mapSize.width = 1024; // Largeur de la carte d'ombre
@@ -111,14 +127,9 @@ const cameraFinalPosition = new THREE.Vector3(0, 0.3, 2);
 
 const cameraDefaultPosition = new THREE.Vector3(0, 1, 10);
 
-cameraDefaultPosition.applyAxisAngle(new THREE.Vector3(1, 0, 0), - Math.PI / 10); 
+cameraDefaultPosition.applyAxisAngle(new THREE.Vector3(1, 0, 0), -Math.PI / 10);
 
-const camera = new THREE.PerspectiveCamera(
-  35,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100);
 camera.position.copy(cameraDefaultPosition);
 scene.add(camera);
 
@@ -149,10 +160,51 @@ window.addEventListener("scroll", () => {
 });
 
 /**
+ * Barre de chargement
+ */
+const loadingBar = document.querySelector(".loading-bar");
+const loadingProgress = document.querySelector(".loading-progress");
+
+function updateLoadingProgress(progress) {
+  loadingProgress.innerHTML =` ${Math.floor(progress)} %`
+}
+
+/**
  * Animate
  */
 const clock = new THREE.Clock();
 let previousTime = 0;
+
+function simulateLoading() {
+  const totalSteps = 100; // Nombre total d'étapes de chargement
+  let currentStep = 0; // Étape de chargement actuelle
+  const loadingWrapper = document.querySelector('.loading__wrapper')
+
+  const interval = setInterval(() => {
+    currentStep++;
+
+    // Mettre à jour la barre de chargement
+    const progress = (currentStep / totalSteps) * 100;
+    updateLoadingProgress(progress);
+
+    if (currentStep === totalSteps) {
+      // Toutes les étapes de chargement sont terminées
+      clearInterval(interval);
+
+      // Ajoutez une classe CSS pour masquer la barre de chargement
+      loadingWrapper.classList.add("loading-complete");
+
+      // Attendre un court délai avant de charger la scène
+      setTimeout(() => {
+        // Charger la scène et afficher
+        loadModels();
+      }, 500); // Délai en millisecondes avant de charger la scène
+    }
+  }, 50); // Intervalle de mise à jour de la barre de chargement (en millisecondes)
+}
+
+
+simulateLoading();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
@@ -173,14 +225,11 @@ const tick = () => {
   // Update controls
   //controls.update();
 
-
   // Render
   renderer.render(scene, camera);
-  
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
 };
 
 tick();
-
